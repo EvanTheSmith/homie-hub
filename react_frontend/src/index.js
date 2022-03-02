5cbb8283fa54bf969f02d0ca293d1e230974d03b
EvanTheSmith <ProfessionalEmail@Zoho.com>
2022-03-02 00:58:44 -0600

    Added basic framework for store, thunk and reducer usage in Index.js

--- react_frontend/src/index.js
+++ react_frontend/src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { composeWithDevTools } from 'redux-devtools-extension'; // for testing in Chrome
+
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import homieReducer from './reducers/homieReducer';
 
-// const store = createStore(homieHubReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(homieReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}><App /></Provider>,
   document.getElementById('root')
 );
 
